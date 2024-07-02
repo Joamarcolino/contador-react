1bd1a4c42b5491d6adaabc50c5aa34b6c92daafc
João Vitor Marcolino Veiga <166542665+Joamarcolino@users.noreply.github.com>
2024-07-02 16:48:54 -0300

    Update contador.js

--- src/components/contador.js
+++ src/components/contador.js
@@ -3,38 +3,40 @@ import { useState, useEffect } from "react"
 
 
 function Contador() {
-  const [fundo, setFundo] = useState(null)
-  const [vitoria, setVitoria] = useState(null)
   let [numero, setNumero] = useState(0)
   const [Brilho, setBrilho] = useState('')
 
+  // dar brilho quando chegar ao 1 milhão positivo e negativo respectivamente.
+ useEffect(()=>{
+    if (numero > 999999){
+        setBrilho('positivebright')
+    } else if (numero > 999999){
+        setBrilho('negativebright')
+    } else {
+        setBrilho('')
+    }
+   } , [numero])
+
+   useEffect(()=>{
+    if (numero > 999999 || numero > 999999){
+        const tempo = (setTimeout(() => {
+            setNumero(0)
+        }, 1000)) //espera 2 minutos antes de resetar o numero
+        return () => clearTimeout(tempo)
+    }
+   } , [numero])
+   
 
   //código do muda cor de fundo de acordo com o valor positivo, negativo e neutro do estado da aplicação
-  
-function Fundo(){
-   return('')
-}
-
-
-  if (add > 0){
-    setFundo(true)
-  } else if(add < 0){
-    setFundo(false)
-  } else if (add = numero){
-    setFundo(null)
-  } 
-
-
-  if (Fundo(true)) {
-    document.body.className ('positive')
-  } else if(Fundo(false)) {
-    document.body.className ('negative')
-  } else if (Fundo(null)) {
-    document.body.className ('neutral')
-  } else {
-    document.body.className = ''
-  }
-    
+  useEffect(() => {
+    if (numero > 0) {
+      document.body.className = 'positive';
+    } else if (numero < 0) {
+      document.body.className = 'negative';
+    } else {
+      document.body.className = 'neutral';
+    }
+  }, [numero]);
 
 
  // mudar o valor do numero no console.
@@ -45,43 +47,32 @@ function Fundo(){
     };
   }, []);
   
-  //muda valor / cheque de valor
-  function add (value) {
-    const valornumero = (numero + value)   
-     setNumero(numero + value)
-  } 
-  
-   if (add >= 1000000){
-      setVitoria(false)
-    } else if(add>= -1000000){
-      setVitoria(true)
-    }
-
-
+  //muda valor
+  function add(value) {
+    setNumero(numero + value)
+  }
 
   return (
-    <body>
-      <section>
-        <header className= 'title'>
-          <h1>Ｃｏｎｔａｄｏｒ</h1>
-        </header>
-        <main className= 'calc'>
-          <p className= {Brilho}>{numero}</p>
-        </main>
-        <footer className='buttons'>
-          <button className='menos1000'onClick={()=>add(-1000)}>-1000</button>
-          <button className='menos100'onClick={()=>add(-100)}>-100</button>
-          <button className='menos10'onClick={()=>add(-10)}>-10</button>
-          <button className='menos' onClick={()=>add(-1)}>-1</button>
-          <button className='zerar' onClick={()=> setNumero(0)}>RESET</button>
-          <button className='mais' onClick={()=>add(1)}>+1</button>
-          <button className='mais10' onClick={()=>add(10)}>+10</button>
-          <button className='mais100' onClick={()=>add(100)}>+100</button>
-          <button className='mais1000' onClick={()=>add(1000)}>+1000</button>
-        </footer>
-      </section>
-    </body>
- )
+    <section>
+      <header className= 'title'>
+        <h1>Ｃｏｎｔａｄｏｒ</h1>
+      </header>
+      <main className= 'calc'>
+        <p className= {Brilho}>{numero}</p>
+      </main>
+      <footer className='buttons'>
+        <button className='menos1000'onClick={()=>add(-1000)}>-1000</button>
+        <button className='menos100'onClick={()=>add(-100)}>-100</button>
+        <button className='menos10'onClick={()=>add(-10)}>-10</button>
+        <button className='menos' onClick={()=>add(-1)}>-1</button>
+        <button className='zerar' onClick={()=> setNumero(0)}>RESET</button>
+        <button className='mais' onClick={()=>add(1)}>+1</button>
+        <button className='mais10' onClick={()=>add(10)}>+10</button>
+        <button className='mais100' onClick={()=>add(100)}>+100</button>
+        <button className='mais1000' onClick={()=>add(1000)}>+1000</button>
+      </footer>
+    </section>
+  )
 } 
 
 export{Contador}
